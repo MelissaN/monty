b8c91d9598491c551110180df31d4fb619992fdf
MelissaN <majajachiu@yahoo.com>
2018-04-26 19:20:57 +0000

    handle swap if only two nodes

--- newposition.c
+++ newposition.c
@@ -14,26 +14,18 @@ void swap(stack_t **h, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*h)->next;
-	(*h)->next = tmp->next;
-	(*h)->next->prev = *h;
+	if (tmp->next != NULL)
+	{
+		(*h)->next = tmp->next;
+		(*h)->next->prev = *h;
+
+	}
+	else
+	{
+		tmp->prev->prev = tmp;
+		tmp->prev->next = NULL;
+	}
 	tmp->prev = NULL;
 	tmp->next = *h;
 	(*h) = tmp;
-}
-/**
- * _add - adds the top two elements in stack
- * @h: top node
- * @line_number: node number
- */
-void _add(stack_t **h, unsigned int line_number)
-{
-	if (*h == NULL || (*h)->next == NULL)
-	{
-		printf("L%u: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	(*h)->next->n += (*h)->n;
-	(*h) = (*h)->next;
-	free((*h)->prev);
-	(*h)->prev = NULL;
 }
